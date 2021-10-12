0f8eed564b369744020ce163412a758acc2f566c
steffanciprian <steffanciprian@gmail.com>
2021-10-12 22:03:01 +0300

    added image url

--- src/components/RentBookForm.js
+++ src/components/RentBookForm.js
@@ -13,85 +13,90 @@ class RentBookForm extends Component {
     }
 
     render() {
-        const {canShowName, bookToRentOrBuy} = this.props;
+        const {canShowName, bookToRentOrBuy, student} = this.props;
 
         return (
             <div style={{alignSelf: 'center', justifySelf: 'center'}}>
 
                 {canShowName ?
-                    <div className='container-dates'>
-                        <div style={{marginInline: '1vw'}}>
-                            <p>Data ridicare</p>
-                            <DatePicker
-                                selected={this.state.dateRent}
-                                onSelect={date => {
-                                    this.setState({dateRent: date})
-                                    console.log(this.state.dateRent)
-                                }} //when day is clicked
-                                onChange={date => console.log(date)} //only when value has changed
-                            />
+                    <div>
+                        <div className='container-dates'>
+                            <div style={{marginInline: '1vw'}}>
+                                <p>Data ridicare</p>
+                                <DatePicker
+                                    selected={this.state.dateRent}
+                                    onSelect={date => {
+                                        this.setState({dateRent: date})
+                                        console.log(student.validForRental)
+                                    }} //when day is clicked
+                                    onChange={date => console.log(date)} //only when value has changed
+                                />
+                            </div>
+
+                            <div style={{marginInline: '1vw'}}>
+                                <p>Data returnare</p>
+                                <DatePicker
+                                    selected={this.state.dateReturn}
+                                    onSelect={date => {
+                                        this.setState({dateReturn: date})
+                                        console.log(this.state.dateReturn)
+                                    }} //when day is clicked
+                                    onChange={date => console.log(date)} //only when value has changed
+                                />
+                            </div>
                         </div>
+                        <div className='container-book-card'>
+                            <img
+                                src={bookToRentOrBuy.url}
+                                style={{height: '60%', width: '80%'}}/>
 
-                        <div style={{marginInline: '1vw'}}>
-                            <p>Data returnare</p>
-                            <DatePicker
-                                selected={this.state.dateReturn}
-                                onSelect={date => {
-                                    this.setState({dateReturn: date})
-                                    console.log(this.state.dateReturn)
-                                }} //when day is clicked
-                                onChange={date => console.log(date)} //only when value has changed
-                            />
+                            <div className='container-price' style={{fontWeight: 'bolder', fontSize: '1.5vw'}}>
+                                {bookToRentOrBuy.title}
+                            </div>
+                            <div className='container-books' style={{fontWeight: 'bold', fontSize: '1vw',}}>
+                                {bookToRentOrBuy.author}
+                            </div>
+
+                            <div className='buttons-container'>
+                                    <Button
+                                        disabled={!student.validForRental}
+                                        onClick={() => {
+                                            this.props.setBookToBuyOrRentDispatch(bookToRentOrBuy)
+                                        }}
+                                        type="primary">
+                                        Inchiriati
+                                    </Button>
+                            </div>
+                            <p>{bookToRentOrBuy.stock} carti in stoc</p>
+
                         </div>
-                    </div> : null}
-
-                <div className='container-book-card'>
-                    <img
-                        src={bookToRentOrBuy.url}
-                        style={{height: '60%', width: '80%'}}/>
-
-                    <div className='container-price' style={{fontWeight: 'bolder', fontSize: '1.5vw'}}>
-                        {bookToRentOrBuy.title}
-                    </div>
-                    <div className='container-books' style={{fontWeight: 'bold', fontSize: '1vw',}}>
-                        {bookToRentOrBuy.author}
                     </div>
 
-                    <div className='buttons-container'>
-                        <Button onClick={() => {
-                            // history.push('/inchiriaza-o-carte')
-                            this.props.setBookToBuyOrRentDispatch(bookToRentOrBuy)
-                        }}
-                                style={{backgroundColor: '#333d51', color: 'white'}}
-                        >
-                            Inchiriati
-                        </Button>
+                    : null}
+
                     </div>
-                    <p>{bookToRentOrBuy.stock} carti in stoc</p>
-
-                </div>
-
-
-            </div>
-        )
-    }
+                    )
+                }
 
 }
 
-const mapStateToProps = state => ({
-        student: state.setStudentReducer.student,
-        canShowName: state.setCanShowNameReducer.canShowName,
-        bookToRentOrBuy: state.setBookToRentOrBuyReducer.bookToRentOrBuy,
+const mapStateToProps = state => ( {
+    student: state.setStudentReducer.student
+,
+    canShowName: state.setCanShowNameReducer.canShowName
+,
+    bookToRentOrBuy: state.setBookToRentOrBuyReducer.bookToRentOrBuy
+,
 
-    }
+}
 
 )
 
 const mapDispatchToProps = dispatch => bindActionCreators(
-    {
-        setBookToBuyOrRentDispatch: setBookToBuyOrRentDispatch,
-    }
-    , dispatch)
+{
+    setBookToBuyOrRentDispatch: setBookToBuyOrRentDispatch,
+}
+, dispatch)
 
 export default connect(mapStateToProps,
-    mapDispatchToProps)(withRouter(RentBookForm));
+mapDispatchToProps)(withRouter(RentBookForm));
