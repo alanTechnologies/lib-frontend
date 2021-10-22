999044712b5440d1f269ec306fb65ef0aaf304f3
steffanciprian <steffanciprian@gmail.com>
2021-10-22 20:19:21 +0300

    started to workn on the return a book page

--- src/pages/ReturnABookPage.js
+++ src/pages/ReturnABookPage.js
@@ -4,11 +4,15 @@ import {connect} from 'react-redux';
 import {Button, Input} from "antd";
 import setStudentDispatch from "../redux/dispatch/SetStudentDispatch";
 import setCanShowNameDispatch from "../redux/dispatch/SetCanShowNameDispatch";
+import fetchRentBooksByStudentCnpDispatch from "../redux/dispatch/FetchRentBooksByStudentCnpDispatch";
 import setNotRecognizedCnpDispatch from "../redux/dispatch/SetNotRecognizedCnp";
+import BookCard from "../components/BookCard";
+import PaginationList from "react-pagination-list";
 
 class ReturnABookPage extends Component {
 
     render() {
+        const {rentBooks} = this.props;
         const onChange = event => {
             this.setState({
                 cnp: event.target.value,
@@ -17,7 +21,6 @@ class ReturnABookPage extends Component {
         }
 
         const getRentBookOfStudentByStudentCnp = cnp => {
-
             return fetch('http://localhost:8080/student-rent-book-list/' + cnp)
                 .then(r => r.json())
                 .then(r=> console.log(r))
@@ -28,15 +31,28 @@ class ReturnABookPage extends Component {
         return (
             <div className='return-book-container'>
                 <Input onChange={event => onChange(event)} placeholder='Introduceti CNP' className='number'/>
-                <Button onClick={() => getRentBookOfStudentByStudentCnp(this.state.cnp)} type="primary"
+                <Button onClick={() =>
+                    // getRentBookOfStudentByStudentCnp(this.state.cnp)
+                    this.props.fetchRentBooksByStudentCnpDispatch(this.state.cnp)
+                } type="primary"
                         className='button-style-2'>Valideaza</Button>
+
+
+                <PaginationList
+                    data={rentBooks}
+                    pageSize={3}
+                    renderItem={(rentBook) => (
+                        <BookCard book={rentBook.book}/>
+                    )}
+                />
+
             </div>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    books: state.setBooksReducer.books,
+    rentBooks: state.fetchRentBooksReducer.rentBooks,
     student: state.setStudentReducer.student,
     canShowName: state.setCanShowNameReducer.canShowName,
 })
@@ -45,6 +61,7 @@ const mapDispatchToProps = dispatch => bindActionCreators({
     setStudentDispatch: setStudentDispatch,
     setCanShowNameDispatch: setCanShowNameDispatch,
     setNotRecognizedCnpDispatch: setNotRecognizedCnpDispatch,
+    fetchRentBooksByStudentCnpDispatch: fetchRentBooksByStudentCnpDispatch,
 
 }, dispatch)
 
